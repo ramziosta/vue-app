f8ecb2f534fddae3f1f4c8ce8ed497c9e177ab93
ramziosta <ramziosta@gmail.com>
2025-03-14 15:17:57 +0300

    edits to fe

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,17 @@
-import {fileURLToPath, URL} from 'node:url'
-import tailwindcss from '@tailwindcss/vite'
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue({
             template: {
                 compilerOptions: {
-                    isCustomElement: tag => tag === 'Section'
-                }
-            }
+                    isCustomElement: (tag) => tag === 'Section',
+                },
+            },
         }),
         vueDevTools(),
         tailwindcss(),
@@ -23,14 +22,13 @@ export default defineConfig({
             '/api': {
                 target: 'http://localhost:8000',
                 changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '') // This removes /api prefix
-            }
-        }
-    },
-
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+                rewrite: (path) => path.replace(/^\/api/, ''), // This removes /api prefix
+            },
         },
     },
-})
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+});
