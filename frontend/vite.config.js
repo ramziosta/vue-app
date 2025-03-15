d466939eba905a69def472731f78bb9394035060
ramziosta <ramziosta@gmail.com>
2025-03-15 13:50:54 +0300

    backend deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:8000',
+                target: 'https://vue-app-backend.vercel.app/',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ''), // This removes /api prefix
             },
